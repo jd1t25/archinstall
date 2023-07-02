16e5677f7fe2a4b98c6916581b7ca75f9887a5a9
jd1t <70378282+jd1t25@users.noreply.github.com>
2023-07-03 00:16:03 +0530

    Update and rename next.sh to post.sh

--- post.sh
+++ post.sh
@@ -1,8 +1,7 @@
-user() {
+post() {
 
 		arch-chroot /mnt su $USER <<-END
-		pacman -S xorg pulseaudio stow git go --noconfirm --needed
-		systemctl enable NetworkManager bluetooth
+		pacman -S xorg pulseaudio stow git --noconfirm --needed
 
 		git clone https://github.com/jd1t25/dotfiles
 		stow -d ~/dotfiles/ -t ~
@@ -10,21 +9,12 @@ user() {
 		stow --adopt -t ~/.config .config
 
 		pacman -Sc --noconfirm
-		sudo pacman -Syu
+		sudo pacman -Syu yay
 		cat ~/dotfiles/backup/pacman.bak | xargs sudo pacman -S --noconfirm --needed 2> ~/packages.log
 
-
-		# Install yay
-		sudo pacman -S --needed base-devel --noconfirm
-		git clone https://aur.archlinux.org/yay.git
-		cd yay
-		makepkg -si
-		cd ..
-		rm -rf yay
-		
 		# Restore foreign packages
 		cat ~/dotfiles/backup/yay.bak | xargs yay -S --needed --noconfirm 2> yay.log
 		END
 }
 
-user 2>&1 | tee user.log
+post 2>&1 | tee post.log
