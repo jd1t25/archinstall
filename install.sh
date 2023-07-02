012138cea7285d94dec0c430184243b1e7a83262
jd1t <70378282+jd1t25@users.noreply.github.com>
2023-07-03 00:16:37 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -105,6 +105,27 @@ next() {
 		chown $USER:$USER /home/$USER
 		echo $PASSWORD | sudo su - $USER
 		END
+  
+  		post
+}
+
+post() {
+
+		arch-chroot /mnt su $USER <<-END
+		pacman -S xorg pulseaudio stow git --noconfirm --needed
+
+		git clone https://github.com/jd1t25/dotfiles
+		stow -d ~/dotfiles/ -t ~
+		cd ~/dotfiles/config
+		stow --adopt -t ~/.config .config
+
+		pacman -Sc --noconfirm
+		sudo pacman -Syu yay
+		cat ~/dotfiles/backup/pacman.bak | xargs sudo pacman -S --noconfirm --needed 2> ~/packages.log
+
+		# Restore foreign packages
+		cat ~/dotfiles/backup/yay.bak | xargs yay -S --needed --noconfirm 2> yay.log
+		END
 }
 		
 
